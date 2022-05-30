48be730a21dbfa97e2473278beab9aaa98b04332
David Cai <davidcai2012@gmail.com>
2022-05-30 12:02:24 -0400

    Initial Commit

---
+++ RepTimer/RepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepTimerApp.swift
+//  RepTimer
+//
+//  Created by David Cai on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct RepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
